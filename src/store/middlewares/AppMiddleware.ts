e3a3a74af70f15e05d225e75fd696030f9834a9e
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2022-08-16 17:25:07 +0500

    AddTodo saga: Add todo to firestore document.

--- src/store/middlewares/AppMiddleware.ts
+++ src/store/middlewares/AppMiddleware.ts
@@ -3,32 +3,28 @@ import { call, delay, put } from 'redux-saga/effects';
 import { AddTodoActions, DeleteTodoActions, UpdateTodoActions } from '../actions/AppAction';
 import { CommonUtils } from '../../config/utils';
 import {Action} from '../actions/ActionCreator';
+import firestore from '@react-native-firebase/firestore';
+import { TodoType } from '../../containers/Home/TodoItem';
 
+const TodoCollection = firestore().collection("todo");
 export default class AppMiddleware
 {
 
     static *AddTodo (action:Action)
     {
         const {Success, Failure, } = AddTodoActions;
-
+        const todo: TodoType = action.payload as TodoType;
         try
         {
             //some api call
             // success can be true or false randomly
-            let success: number | boolean = yield delay(10000, CommonUtils.randomNumber(0, 2))
-            success = Boolean(success);
-            if (success)
-            {
-                yield put(Success(action.payload))
-                yield call(()=>action.cb?.());
-            }
-            else
-            {
-                yield put(Failure({}))
-            }
+            yield TodoCollection.doc(todo.id.toString()).set(todo);
+            yield put(Success(action.payload))
+            if (action.cb) yield call<any>(action.cb);
         }
         catch (err)
         {
+            console.log("Error e:", err);
             yield put(Failure({}))
         }
     }
