093c113ba767fd905e54cf49405c7ad18de335be
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2022-08-19 15:01:52 +0500

    Use takeEvery for AddTodo, and UpdateTodo actions.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,10 @@
 import { AppMiddleware } from "../middlewares"
-import { takeLatest, all } from 'redux-saga/effects'
+import { takeLatest, all, takeEvery } from 'redux-saga/effects'
 import { AddTodoActions, FetchAllTodoActions, UpdateTodoActions } from "../actions/AppAction"
 export function* Sagas() {
     yield all([
-        yield takeLatest(AddTodoActions.Default.type, AppMiddleware.AddTodo),
-        yield takeLatest(UpdateTodoActions.Default.type, AppMiddleware.UpdateTodo),
+        yield takeEvery(AddTodoActions.Default.type, AppMiddleware.AddTodo),
+        yield takeEvery(UpdateTodoActions.Default.type, AppMiddleware.UpdateTodo),
         yield takeLatest(FetchAllTodoActions.Default.type, AppMiddleware.FetchAllTodo),
     ])
 }
